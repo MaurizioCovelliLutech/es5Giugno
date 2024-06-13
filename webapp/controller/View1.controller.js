eb432a9173c623f2ef392c4879ccd5c97949f855
Maurizio Covelli <maurizio.covelli@lutech.it>
2024-06-13 13:01:20 +0000

    secondo

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,9 +7,11 @@ sap.ui.define([
 	"sap/ui/model/odata/v2/ODataModel",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/Fragment",
-	"sap/m/MessageBox"
+	"sap/m/MessageBox",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 
-], function(Controller, SearchField, Dialog, List, StandardListItem,ODataModel,JSONModel,Fragment,MessageBox) {
+], function(Controller, SearchField, Dialog, List, StandardListItem,ODataModel,JSONModel,Fragment,MessageBox,Filter,FilterOperator) {
     "use strict";
 
     const sURL = "/V2/Northwind/Northwind.svc/";
@@ -20,6 +22,48 @@ sap.ui.define([
 
 			const oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("RouteView1").attachPatternMatched(this.onRouteMatched, this);
+        },
+
+        onRouteMatched: async function () {
+
+            var oModel = new ODataModel(sURL);
+
+            const oData = await new Promise((resolve, reject) => {
+              oModel.read("/Customers", {
+              success: function(oData, response) {
+                resolve(oData);
+              },
+              error: function(error) {
+              }
+        });
+    });  
+    
+             this.getView().setModel(new JSONModel(oData.results), "modCustomer");
+
+        },
+
+        onTestSearch: function(oEvent) {
+
+            var sQuery = oEvent.getSource().getValue();
+            var oBinding = this.byId("customerTab").getBinding("items")
+
+            var aFilters = [];
+
+            if (sQuery) {
+                aFilters.push(new Filter("CustomerID", FilterOperator.Contains, sQuery));
+                aFilters.push(new Filter("CompanyName", FilterOperator.Contains, sQuery));
+                aFilters.push(new Filter("City", FilterOperator.Contains, sQuery));
+                aFilters.push(new Filter("Fax", FilterOperator.Contains, sQuery));
+                oBinding.filter(new Filter(aFilters, false));
+            } else {
+                oBinding.filter([]);
+            }
+        },
+
+        onNavigazione: function(){
+
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteView2");
         }
     })
 
